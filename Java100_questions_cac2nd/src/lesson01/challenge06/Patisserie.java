5b8fe50be7bff71a7cccb0a08546134b5fad6b92
zijian <zijian@C010423722.zipangu.cac.co.jp>
2026-04-15 16:37:10 +0900

    lesson01.challenge06-10完了

--- Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
+++ Java100_questions_cac2nd/src/lesson01/challenge06/Patisserie.java
@@ -21,6 +21,48 @@
 
 package lesson01.challenge06;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Patisserie {
+	public static void main(String[] args) throws IOException {
+		int CitroPrice = 250;
+		int ChocolatPrice = 280;
+		int PistachePrice = 320;
+
+		System.out.println("\nそれぞれ何個ずつ買いますか？（最大30個まで）\n");
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+		System.out.print("シトロン      >");
+		String Citro = reader.readLine();
+		int CitronBuyCount = Integer.parseInt(Citro);
+		System.out.print("ショコラ      >");
+		String Chocolat = reader.readLine();
+		int ChocolatBuyCount = Integer.parseInt(Chocolat);
+		System.out.print("ピスターシュ  >");
+		String Pistache = reader.readLine();
+		int PistacheBuyCount = Integer.parseInt(Pistache);
+
+		int allCount = CitronBuyCount + ChocolatBuyCount + PistacheBuyCount;
+		int allPrice = CitroPrice * CitronBuyCount + ChocolatPrice * ChocolatBuyCount
+				+ PistachePrice * PistacheBuyCount;
+
+		System.out.println("\nシトロン     " + CitronBuyCount + "個");
+		System.out.println("ショコラ     " + ChocolatBuyCount + "個");
+		System.out.println("ピスターシュ " + PistacheBuyCount + "個\n");
+
+		System.out.println("合計個数    " + allCount + "個");
+		System.out.println("合計金額  " + allPrice + "円");
+
+		System.out.println("\nをお買いあげですね。\n承りました。\n");
+
+		System.out.println("本日のおすすめ商品です。\n");
+
+		System.out.println("シトロン      \\250 ・・・ 残り" + (30 - CitronBuyCount) + "個");
+		System.out.println("ショコラ      \\280 ・・・ 残り" + (30 - ChocolatBuyCount) + "個");
+		System.out.println("ピスターシュ  \\320 ・・・ 残り" + (30 - PistacheBuyCount) + "個");
+
+	}
 
 }
